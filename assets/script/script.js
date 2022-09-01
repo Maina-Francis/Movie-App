9afb3a16e8a14d390183bee255d11b813a0252f9
Francis Maina <sebastianfrancix@gmail.com>
2022-09-01 13:07:10 +0300

    appends movie images, titles and ratings to the body

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,7 +5,7 @@ const apiUrl =
   "https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1&with_watch_monetization_types=flatrate";
 
 //Image path provided in the tmdb chat forum https://www.themoviedb.org/talk/5aeaaf56c3a3682ddf0010de
-const imagePath = "https://image.tmdb.org/t/p/w1280/";
+const imagePath = "https://image.tmdb.org/t/p/w1280";
 
 async function getMovies() {
   // The await operator is used to wait for a Promise. It can only be used inside an async function within regular JavaScript code.
@@ -15,12 +15,30 @@ async function getMovies() {
   console.log(responseData);
 
   //Getting the movie images and appending it to the body
+
   //   responseData.results.forEach((movie) => {
   //     let image = document.createElement("img");
   //     image.src = imagePath + movie.poster_path;
 
   //     document.body.appendChild(image);
   //   });
+
+  //alternatively appending the images to the body after specifying its css properties
+  responseData.results.forEach((movie) => {
+    let movieElement = document.createElement("div");
+    movieElement.className = "movies";
+
+    movieElement.innerHTML = `
+        <img
+        src="${imagePath + movie.poster_path}" alt="${movie.title}" />
+
+      <div class="movie-details">
+        <h3>${movie.title}</h3>
+        <span>${movie.vote_average}</span>
+      </div>`;
+
+    document.body.appendChild(movieElement);
+  });
 
   return responseData;
 }
