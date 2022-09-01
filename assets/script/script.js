e8c74007f06047a45f39ce4b619b035f8187ff64
Francis Maina <sebastianfrancix@gmail.com>
2022-09-01 10:55:51 +0300

    added the html structure

---
+++ assets/script/script.js
@@ -0,0 +1,2 @@
+//API KEY 68215e02536f64517172acad103660ce
+// API URL = https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=500&with_watch_monetization_types=flatrate
