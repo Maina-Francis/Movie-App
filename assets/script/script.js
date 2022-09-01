43ab0d83b6f2067bd863c621471f259268e98530
Francis Maina <sebastianfrancix@gmail.com>
2022-09-01 11:28:01 +0300

    fetches data from the api

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,20 @@
 //API KEY 68215e02536f64517172acad103660ce
-// API URL = https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=500&with_watch_monetization_types=flatrate
+// API URL = https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1&with_watch_monetization_types=flatrate
+
+const apiUrl =
+  "https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1&with_watch_monetization_types=flatrate";
+
+//Image path provided in the tmdb chat forum https://www.themoviedb.org/talk/5aeaaf56c3a3682ddf0010de
+const imagePath = "https://image.tmdb.org/t/p/w1280/";
+
+async function getMovies() {
+  // The await operator is used to wait for a Promise. It can only be used inside an async function within regular JavaScript code.
+  const response = await fetch(apiUrl);
+  const responseData = await response.json();
+
+  console.log(responseData);
+
+  return responseData;
+}
+
+getMovies();
