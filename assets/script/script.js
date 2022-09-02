84a7a4afa6e9384610b6276761bff60b5ed509fb
Francis Maina <sebastianfrancix@gmail.com>
2022-09-02 22:47:06 +0300

    adds the header section

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,8 @@
 //API KEY 68215e02536f64517172acad103660ce
-// API URL = https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1&with_watch_monetization_types=flatrate
+// API URL = https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
 
 const apiUrl =
-  "https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=true&page=1&with_watch_monetization_types=flatrate";
+  "https://api.themoviedb.org/3/discover/movie?api_key=68215e02536f64517172acad103660ce&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_watch_monetization_types=flatrate";
 
 //Image path provided in the tmdb chat forum https://www.themoviedb.org/talk/5aeaaf56c3a3682ddf0010de
 const imagePath = "https://image.tmdb.org/t/p/w1280";
